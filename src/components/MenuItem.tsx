7e9552d3bb33c84ef500ee723372ffc251bd6cfb
Luis-Alvarezz <luisangelug10@gmail.com>
2025-10-30 09:26:07 -0600

    [MODIFY]: Cambio de Bg a los Item del Menu

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -9,7 +9,7 @@ type MenuItemProps = {
 
 export default function MenuItem({item, dispatch}: MenuItemProps) {
   return (
-    <button className="border-2 border-teal-400 hover: bg-teal-200 w-full p-3 flex justify-between" onClick={() => dispatch({type: 'addItem', payload: {item: item} })}>
+    <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between" onClick={() => dispatch({type: 'addItem', payload: {item: item} })}>
       <p>{item.name}</p>
       <p className="font-black">${item.price}</p>
     </button>
