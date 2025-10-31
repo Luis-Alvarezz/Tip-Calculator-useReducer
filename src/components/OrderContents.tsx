4fb5bed6002e9d5df8a21f2ca423fc54957bc4b6
Luis-Alvarezz <luisangelug10@gmail.com>
2025-10-31 09:07:43 -0600

    [MODIFY]: Logica para renderizar componente con su Tipado

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,38 +1,39 @@
 import { formatCurrency } from "../helpers"
-import type { MenuItem, OrderItem } from "../types/types"
+import type { OrderActions } from "../reducers/cal-prop-reducer"
+import type { OrderItem } from "../types/types"
 
 type OrderContentsProps = {
   order: OrderItem[],
-  removeItem: (id: MenuItem['id']) => void
+  dispatch: React.Dispatch<OrderActions>
 }
 
-export default function OrderContents({ order, removeItem }: OrderContentsProps) {
+export default function OrderContents({ order, dispatch }: OrderContentsProps) {
   return (
     <div>
-      <h2 className='font-black text-4xl'>Consumo</h2>
+      <h2 className="font-black text-4xl">Consumo</h2>
 
       <div className="space-y-3 mt-10">
-        {order.map(item => (
-          <div key={item.id} className="flex justify-between items-center border-t border-gray-300 py-5 last-of-type:border-b">
-            <div>
+        {
+          order.map( item => (
+            <div key={ item.id } className="flex justify-between items-center border-t border-gray-300 last-of-type:border-b">
+              <div>
               <p className="text-lg">
-                {item.name} - {formatCurrency(item.price)}
+                { item.name } - {formatCurrency(item.price)}
               </p>
               <p className="font-black">
-                Cantidad: {item.quantity} - {formatCurrency(item.price * item.quantity)}
+                Cantidad: { item.quantity } - { formatCurrency(item.price * item.quantity) }
               </p>
-            </div>
-
-            <div>
-              <button
-                className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
-                onClick={() => removeItem(item.id)}
-              >
-                X
-              </button>
-            </div>
-          </div>
-        ))
+              </div>
+              <div>
+                <button
+                  className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
+                  onClick={() => dispatch({type: 'removeItem', payload: {item: item.id}})}
+                >
+                  X
+                </button>
+              </div>
+            </div> // ! .key={ item.id }
+          ))
         }
       </div>
     </div>
